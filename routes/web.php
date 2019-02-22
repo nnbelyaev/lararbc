777119f784b0503372c4030115eb44c5ac5c2702
Nikolay Belyaev <nnbelyaev@gmail.com>
2019-02-22 21:18:12 +0200

    localization and route configure complete!

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,19 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => LaravelLocalization::setLocale()], function()
+{
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    Auth::routes();
+    Route::get('/home', 'HomeController@index')->name('home');
 });
 
-Auth::routes();
 
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+
+
+
+
