2c5987e8875f0ed923dabc8b327dcdeb4a4bb0f7
Nikolay Belyaev <nnbelyaev@gmail.com>
2019-02-22 20:31:47 +0200

    rubric factory and seeder

--- app/Rubric.php
+++ app/Rubric.php
@@ -3,8 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Rubric extends Model
 {
-    //
+    protected $guarded = [];
+
+    public static function boot() {
+        parent::boot();
+
+        static::creating(function ($rubric) {
+            $rubric->translit = Str::slug($rubric->name_rus);
+        });
+    }
+
+
 }
--- database/factories/RubricFactory.php
+++ database/factories/RubricFactory.php
@@ -3,7 +3,25 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Rubric::class, function (Faker $faker) {
+    $faker->locale('ru_RU');
+
+    $city = $faker->unique()->city;
     return [
-        //
+        'status' => 'normal',
+        'category' => $faker->randomElement(['news', 'styler', 'afisha', 'lite', 'all']),
+        'name_rus' => $city,
+        'name_ukr' => $city,
+        'h1_rus' => $city,
+        'h1_ukr' => $city,
+        'seo_title_rus' => $city,
+        'seo_title_ukr' => $city,
+        'seo_key_rus' => $faker->sentence(6),
+        'seo_key_ukr' => $faker->sentence(6),
+        'seo_descr_rus' => $faker->sentence(6),
+        'seo_descr_ukr' => $faker->sentence(6),
+        'banner_zone' => $faker->randomElement(['other', 'business']),
+        'google_news' => $city,
+        'subdomain' => $faker->randomElement([false, true]),
+        'order' => rand(0,100)
     ];
 });
